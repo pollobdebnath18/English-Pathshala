3befcbd91b39dbdadb64b1a62027ab2f414368bb
pollob debnath <pullobdebnath@gmail.com>
2025-11-13 20:55:49 +0600

    Add modal for details word meaning

--- script/connect.js
+++ script/connect.js
@@ -11,6 +11,39 @@ const loadButton = () => {
     .then((res) => res.json())
     .then((data) => displayButton(data.data));
 };
+
+//load Word Details
+const loadWordDetails = (id) => {
+  const url = `https://openapi.programming-hero.com/api/word/${id}`;
+  console.log(url);
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayWordDetails(data.data));
+};
+
+const displayWordDetails = (details) => {
+  console.log(details);
+  document.getElementById("wordDetails").showModal();
+  const detailsContainer = document.getElementById("details_container");
+  detailsContainer.innerHTML = `
+  <div class="flex justify-center">
+        <div class="card bg-base-50  shadow-sm mt-6">
+          <div class="card-body p-3 pl-6 shadow-xl">
+            <h2 class="card-title text-2xl font-bold">
+              ${details.word} ( <i class="fa-solid fa-microphone"></i>: ${details.pronunciation} )
+            </h2>
+            <h3 class="font-bold text-xl">Meaning</h3>
+            <h3 class="font-bold text-xl">${details.meaning}</h3>
+            <br>
+            <h3 class="font-bold text-xl">Example</h3>
+            <h3 class=" text-lg text-black">${details.sentence}.</h3>
+            <p class="text-lg font bold">সমার্থক শব্দ হলো :</p>
+
+          </div>
+        </div>
+      </div>
+  `;
+};
 function displayButton(button) {
   // console.log(button);
   const lessonButtonContainer = document.getElementById("lesson-btn");
@@ -57,7 +90,7 @@ function displayWordMeaning(words) {
               <p class="text-lg">meaning / pronunciation</p>
               <p class="text-lg font-bold">${word.meaning} / ${word.pronunciation}</p>
               <div class="flex justify-between items-center py-5">
-                <button class="btn"><i class="fa-solid fa-clock text-xl"></i></button>
+                <button  onclick=loadWordDetails(${word.id}) class="btn"><i class="fa-solid fa-clock text-xl"></i></button>
                 <button class="btn"> <i class="fa-solid fa-volume-high text-xl"></i></button>
               </div>
             </div>
@@ -70,6 +103,7 @@ function displayWordMeaning(words) {
     wordMeaning.appendChild(wordDiv);
   }
 }
+
 //Load category card (api-4)
 const loadCategoryCard = (id) => {
   const url = `https://openapi.programming-hero.com/api/level/${id}`;
